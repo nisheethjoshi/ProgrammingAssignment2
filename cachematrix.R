012d4da7eb56b0ab6a54704cc11a5fe3a135faae
Nisheeth Joshi <nisheeth.joshi@rediffmail.com>
2016-07-10 16:00:21 +0530

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -46,38 +46,3 @@ inverse <- pseudoinverse(data, ...)
 X$setinverse(inverse)
 inverse
 }
-
-
-#Experiment to try if it works
-#square matrix
-X <- matrix(rpois(25,3), nrow = 5)
-cX <- makeCacheMatrix(X)
-cX$get()
-cacheSolve(cX)
-cacheSolve(cX)
-invX <- cacheSolve(cX)
-
-#Experiment to try if it works
-#rectangular matrix rows > cols
-Y <- matrix(rpois(20,2), nrow = 5, ncol = 4)
-cY <- makeCacheMatrix(Y)
-cY$get()
-cacheSolve(cY)
-cacheSolve(cY)
-invY <- cacheSolve(cY)
-
-#Experiment to try if it works
-#rectangular matrix rows < cols
-Z <- matrix(rpois(20,1), nrow = 4, ncol = 5)
-cZ <- makeCacheMatrix(Z)
-cZ$get()
-cacheSolve(cZ)
-cacheSolve(cZ)
-invZ <- cacheSolve(cZ)
-
-#Experiment to try if it works
-#multiplication must return identity or closer
-invX %*% X 
-X %*% invX
-invY %*% Y 
-Z %*% invZ 
